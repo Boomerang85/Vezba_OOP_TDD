9d9acf13525ef0c0f3c0911d4df2ffea2e6a4544
Boomerang85 <whiteluke06@gmail.com>
2024-04-04 09:53:58 +0200

    Novi projektni setup

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -1,41 +1,14 @@
 public class Point {
 
-    private int x;
-    private int y;
-    private boolean isSelectable;
+    double x;
+    double y;
 
-    public Point(int x, int y) {
+    public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
-    public Point(int x, int y, boolean isSelectable) {
-        this.x = x;
-        this.y = y;
-        this.isSelectable = isSelectable;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public void setIsSelectable(boolean isSelectable) {
-        this.isSelectable = isSelectable;
-    }
-
-    public boolean getIsSelectable() {
-        return this.isSelectable;
-    }
-
-    public int getX() {
+    public double getX() {
         return this.x;
-    }
-
-    public int getY() {
-        return this.y;
     }
 }
--- src/test/java/PointTest.java
+++ src/test/java/PointTest.java
@@ -3,24 +3,8 @@ import org.junit.jupiter.api.Test;
 
 public class PointTest {
     @Test
-    public void test() {
-        Point test = new Point(6, 7);
-        assertEquals(test.getX(), 6);
-        assertEquals(test.getY(), 7);
-    }
-    @Test
-    public void testSetters() {
-        Point test = new Point(6, 8);
-        test.setX(6);
-        test.setY(8);
-        assertEquals(test.getX(), 6);
-        assertEquals(test.getY(),8);
-    }
-    @Test
-    public void testSelectable() {
-        Point test = new Point(6, 7, true);
-        assertTrue(test.getIsSelectable());
-        test.setIsSelectable(false);
-        assertTrue(!test.getIsSelectable());
+    public void test_getXTrebaDaVrati5AkoJeKlasaInstanciranaSaXjednako5() {
+        Point test = new Point(5, 5);
+        assertEquals(test.getX(), 5);
     }
 }
